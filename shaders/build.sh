0f608c22c47748965719f3a836185685a867d87d
Chris Butler <chrisbutler296@gmail.com>
2022-10-30 14:15:19 -0600

    Embed Metal source in shaders
    This will let us debug, replay, and profile our shaders in Xcode

--- shaders/build.sh
+++ shaders/build.sh
@@ -5,8 +5,11 @@ dir=${1:-.}
 
 set -ex
 
+export MTL_ENABLE_DEBUG_INFO=INCLUDE_SOURCE
+
 # sdk can be: macosx, iphoneos
 xcrun -sdk macosx metal         \
     -frecord-sources=flat       \
+    -gline-tables-only -MO      \
     $SCRIPT_DIR/Shaders.metal   \
     -o $dir/Shaders.metallib
